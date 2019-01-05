25410ff3055b82cf613b5e15eb4d4bb6d0db1302
Gareth Johnson <garethbjohnson@users.noreply.github.com>
2019-01-05 23:30:33 +0000

    Add space background

---
+++ Assets/Code/SpaceBackground.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpaceBackground : MonoBehaviour {
+    void Awake()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
